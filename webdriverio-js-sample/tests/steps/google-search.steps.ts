8a349228218b2fb9854db953a97bdee1e671df29
KennyFrans <franzkenny26995@gmail.com>
2025-07-05 13:13:20 +0700

    done for browser sample

---
+++ webdriverio-js-sample/tests/steps/google-search.steps.ts
@@ -0,0 +1,16 @@
+import { Given, Then, When } from '@wdio/cucumber-framework';
+import googleHomePage from '@pages/google-home.page.ts';
+
+Given("I am on Google home page", async () => {
+    await googleHomePage.navigate();
+    await googleHomePage.waitForIsShown();
+});
+
+When("I search for {string}", async (term: string) => {
+    await googleHomePage.search(term);
+});
+
+Then("I see the results", async () => {
+    let stats = await googleHomePage.getResultStats();
+    expect(stats).toEqual(true);
+});
